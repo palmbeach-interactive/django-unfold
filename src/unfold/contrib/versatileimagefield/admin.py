51e4f397d32607c73f773ced73360448add4618f
Marc Widmer <marc@pbi.io>
2025-03-22 08:14:12 +0100

    Refactor to reuse widget application logic for image fields.

--- src/unfold/contrib/versatileimagefield/admin.py
+++ src/unfold/contrib/versatileimagefield/admin.py
@@ -2,56 +2,44 @@ from django.contrib import admin
 from versatileimagefield.fields import VersatileImageField
 from versatileimagefield.widgets import VersatileImagePPOIClickWidget
 
-
-class UnfoldVersatileImageAdmin(admin.ModelAdmin):
-    def get_form(self, request, obj=None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
-
+class VersatileImageFieldMixin:
+    def _apply_versatile_image_widget(self, form_or_formset):
         # Get all VersatileImageFields from the model
         image_fields = [
             field.name for field in self.model._meta.fields
             if isinstance(field, VersatileImageField)
         ]
+
+        # Get the base fields from either form or formset
+        base_fields = (
+            form_or_formset.base_fields if hasattr(form_or_formset, 'base_fields')
+            else form_or_formset.form.base_fields
+        )
 
         # Apply widget to each VersatileImageField
         for field_name in image_fields:
-            if field_name in form.base_fields:
-                form.base_fields[field_name].widget = VersatileImagePPOIClickWidget()
-        return form
+            if field_name in base_fields:
+                base_fields[field_name].widget = VersatileImagePPOIClickWidget()
+
+        return form_or_formset
+
+class UnfoldVersatileImageAdmin(VersatileImageFieldMixin, admin.ModelAdmin):
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        return self._apply_versatile_image_widget(form)
 
 
-class UnfoldVersatileImageTabularInline(admin.TabularInline):
+
+class UnfoldVersatileImageTabularInline(VersatileImageFieldMixin, admin.TabularInline):
     def get_formset(self, request, obj=None, **kwargs):
         formset = super().get_formset(request, obj, **kwargs)
-
-        # Get all VersatileImageFields from the model
-        image_fields = [
-            field.name for field in self.model._meta.fields
-            if isinstance(field, VersatileImageField)
-        ]
-
-        # Apply widget to each VersatileImageField in the form
-        for field_name in image_fields:
-            if field_name in formset.form.base_fields:
-                formset.form.base_fields[field_name].widget = VersatileImagePPOIClickWidget()
-
-        return formset
+        return self._apply_versatile_image_widget(formset)
 
 
-class UnfoldVersatileImageStackedInline(admin.StackedInline):
+
+class UnfoldVersatileImageStackedInline(VersatileImageFieldMixin, admin.StackedInline):
     def get_formset(self, request, obj=None, **kwargs):
         formset = super().get_formset(request, obj, **kwargs)
+        return self._apply_versatile_image_widget(formset)
 
-        # Get all VersatileImageFields from the model
-        image_fields = [
-            field.name for field in self.model._meta.fields
-            if isinstance(field, VersatileImageField)
-        ]
-
-        # Apply widget to each VersatileImageField in the form
-        for field_name in image_fields:
-            if field_name in formset.form.base_fields:
-                formset.form.base_fields[field_name].widget = VersatileImagePPOIClickWidget()
-
-        return formset
 
